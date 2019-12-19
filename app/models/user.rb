aa1778d65bea4b8a95e8f7bd5ad81e6789172206
Hanbyul Lee <starrybleu@gmail.com>
2019-12-19 15:56:57 +0900

    User 모델 작성. `has_secure_password` 사용을 위해 gem bcrypt 추가 및 User class 에 `password_digest`라는 이름의 attribute 추가(has_secure_password 의 요구사항이라고 함)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  has_secure_password # encrypt password
+
+  has_many :todos, foreign_key: :created_by
+
+  validates_presence_of :name, :email, :password_digest
 end
